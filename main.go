d461167e4e3aaa29900fafb5d2663fed1a4769fd
erdong01 <88155745@qq.com>
2024-04-24 11:28:50 +0800

    no message

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"log"
-
-	"github.com/armon/go-socks5"
 	"github.com/gcslaoli/go-socks5-server/httpproxy"
 )
 
 func main() {
 	// start http proxy
-	go httpproxy.StartProxy()
+	httpproxy.StartProxy()
 
-	conf := &socks5.Config{}
-	server, err := socks5.New(conf)
-	if err != nil {
-		panic(err)
-	}
-
-	// Create SOCKS5 proxy on localhost port 8000
-	addr := ":1188"
-	log.Printf("Starting socks5 proxy server on %s", addr)
-	if err := server.ListenAndServe("tcp", addr); err != nil {
-		panic(err)
-	}
 }
