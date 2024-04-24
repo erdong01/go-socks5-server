e4838d1c023ff789920f11d30a4abf8715d4bff9
erdong01 <88155745@qq.com>
2024-04-24 11:54:55 +0800

    no message

--- httpproxy/proxy.go
+++ httpproxy/proxy.go
@@ -32,7 +32,7 @@ func handleTunneling(w http.ResponseWriter, r *http.Request) {
 func transfer(destination io.WriteCloser, source io.ReadCloser) {
 	defer destination.Close()
 	defer source.Close()
-	buf := make([]byte, 32*1024) // 32KB buffer
+	buf := make([]byte, 64*1024) // 32KB buffer
 	_, err := io.CopyBuffer(destination, source, buf)
 	if err != nil {
 		log.Printf("Error in transfer: %v", err)
