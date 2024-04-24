c0dcbbf2ce45d163715f03987ae3dfc83b36a2b9
erdong01 <88155745@qq.com>
2024-04-24 12:26:56 +0800

    no message

--- httpproxy/proxy.go
+++ httpproxy/proxy.go
@@ -25,15 +25,14 @@ func handleTunneling(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
 	}
-	go func() {
-		defer destConn.Close()
-		defer clientConn.Close()
-		transfer(destConn, clientConn)
-		transfer(clientConn, destConn)
-	}()
+
+	go transfer(destConn, clientConn)
+	go transfer(clientConn, destConn)
 }
 
 func transfer(destination io.WriteCloser, source io.ReadCloser) {
+	defer destination.Close()
+	defer source.Close()
 	buf := make([]byte, 64*1024) // 32KB buffer
 	_, err := io.CopyBuffer(destination, source, buf)
 	if err != nil {
