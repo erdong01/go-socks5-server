8aef543935399436c17f60a5d54b5d7cf584cd9e
erdong01 <88155745@qq.com>
2024-07-04 12:00:02 +0800

    no message

--- httpproxy/proxy.go
+++ httpproxy/proxy.go
@@ -48,7 +48,7 @@ func transfer(destination io.WriteCloser, source io.ReadCloser, wg *sync.WaitGro
 	defer destination.Close()
 	defer source.Close()
 	buf := bufferPool.Get().([]byte)
-	defer bufferPool.Put(buf[:0])
+	defer bufferPool.Put(buf)
 	io.CopyBuffer(destination, source, buf)
 }
 
